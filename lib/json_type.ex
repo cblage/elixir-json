3b9aa62d1b2da4aed4670dd0c72e77dd76774ca2
Carlos Brito Lage <carlos@carloslage.net>
2013-07-10 23:50:46 -0400

    initial support for keywords, arrays and most atoms

--- lib/json_type.ex
+++ lib/json_type.ex
@@ -24,11 +24,49 @@ defimpl JsonType, for: Tuple do
 end
 
 defimpl JsonType, for: List do
-  def encode(item) do 
-    inspect(item) # failing implementation
+  
+  def encode([]) do 
+    "[]"
   end
 
-   def typeof([]) do 
+  def encode(list) do 
+    if (Keyword.keyword? list) do
+      "{#{_encode_keyword(list, "")}}"
+    else 
+      "[#{_encode_list(list, "")}]"
+    end
+  end
+  
+
+  defp _encode_keyword([], accumulator) when is_bitstring(accumulator) do 
+    accumulator
+  end
+
+  defp _encode_keyword([head|tail], "") do 
+    _encode_keyword(tail, _encode_keyword_item(head))
+  end
+  
+  defp _encode_keyword([head|tail], accumulator) when is_bitstring(accumulator) do 
+      _encode_keyword(tail, accumulator <> "," <> _encode_keyword_item(head))
+  end
+
+  defp _encode_keyword_item({key, object}) do 
+    "\"#{key}\":" <>  JsonType.encode(object)
+  end
+
+  defp _encode_list([], accumulator) when  is_bitstring(accumulator) do 
+    accumulator
+  end
+
+  defp _encode_list([head|tail], "") do 
+    _encode_list(tail, JsonType.encode(head))
+  end
+
+  defp _encode_list([head|tail], accumulator) when is_bitstring(accumulator) do 
+    _encode_list(tail, accumulator <> "," <> JsonType.encode(head))
+  end
+
+  def typeof([]) do 
     :array
   end
  
@@ -63,15 +101,15 @@ end
 
 defimpl JsonType, for: [Atom, BitString] do
    def encode(false) do
-    :false
+    "false"
   end
 
   def encode(true) do
-    :true
+    "true"
   end
   
   def encode(nil) do
-    :null
+    "null"
   end
 
   def encode(atom_or_bitstring) when is_atom(atom_or_bitstring) or is_bitstring(atom_or_bitstring) do 
@@ -83,7 +121,7 @@ defimpl JsonType, for: [Atom, BitString] do
   end
   
   def typeof(nil) do 
-    encode(nil) # should be the same tuple
+    :null
   end
 
   def typeof(atom_or_bitstring) when is_atom(atom_or_bitstring) or is_bitstring(atom_or_bitstring) do
