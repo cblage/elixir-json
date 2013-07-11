9045e46c0af099985b9fb35a688889b1c8a30396
Carlos Brito Lage <carlos@carloslage.net>
2013-07-10 20:55:24 -0400

    very simple test

--- test/json_test.exs
+++ test/json_test.exs
@@ -3,7 +3,15 @@ Code.require_file "test_helper.exs", __DIR__
 defmodule JsonTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert(true)
+  test "convert keyword into correct JSON" do
+    assert \
+      Json.encode([result: "this will be a elixir result"]) \
+      == "{\"result\": \"this will be a elixir result\"}"
+  end
+
+  test "convert JSON into correct keyword" do
+    assert \
+      Json.decode("{\"result\": \"this is awesome\"}") \
+      == [result: "this is awesome"]
   end
 end
