e8fdcd9cb04d02fa82ca8052dcfe90cc4791607b
Carlos Brito Lage <carlos@carloslage.net>
2013-07-12 03:21:51 -0400

    Added test for charlists converting into numerical arrays

--- test/json_test.exs
+++ test/json_test.exs
@@ -3,10 +3,16 @@ Code.require_file "test_helper.exs", __DIR__
 defmodule JsonTest do
   use ExUnit.Case
 
-  test "convert keyword into correct JSON" do
+  test "convert keyword with string into correct JSON" do
     assert \
       Json.encode([result: "this will be a elixir result"]) \
       == "{\"result\":\"this will be a elixir result\"}"
+  end
+  
+  test "convert keyword with charlist into correct JSON" do
+    assert \
+      Json.encode([result: 'this will not be a string']) \
+      == "{\"result\":[116,104,105,115,32,119,105,108,108,32,110,111,116,32,98,101,32,97,32,115,116,114,105,110,103]}"
   end
 
   test "convert complex keyword into correct JSON" do
