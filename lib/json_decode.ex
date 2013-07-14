f865b8b6968936682104836947359d783f52489a
Carlos Brito Lage <carlos@carloslage.net>
2013-07-14 00:07:16 -0400

    Now consuming whitespace. Skeleton for token consumption

--- lib/json_decode.ex
+++ lib/json_decode.ex
@@ -3,8 +3,6 @@ defprotocol JSON.Decode do
   @moduledoc """
   Defines the protocol required for converting Elixir types into JSON and inferring their json types.
   """
-  @only [BitString]
-
   def from_json(item)
 
 end
@@ -14,5 +12,38 @@ defimpl JSON.Decode, for: BitString do
   def from_json("[]") do 
     []
   end
+
+  def from_json("{}") do 
+    []
+  end
+
+  def from_json(bitstring) do 
+    _accept_any_token(bitstring, nil)
+  end
+
+  #Stop condition
+  defp _accept_any_token(<<>>, parent) do
+    parent
+  end
+
+  defp _accept_any_token(bitstring, parent) do
+    _consume_whitespace(bitstring)
+      |>_process_token(parent)
+  end
+
+  # consume whitespace, 32 = ascii space
+  defp _consume_whitespace(<< token, tail :: binary>>) when token in [?\t, ?\r, ?\n, 32] do
+    _consume_whitespace(tail)
+  end
+
+  defp _consume_whitespace(bitstring) when is_bitstring(bitstring) do
+    bitstring
+  end
+
+  defp _process_token(token, _) do
+    raise "Invalid JSON - unexpected token >>#{token}<<"
+  end
+
+
 
 end
