80e9ad4d375dd910ebdb0ce42a8185f4699213f4
James A. Rosen <james@zendesk.com>
2013-07-15 09:41:48 -0700

    Move decoding to its own module

--- lib/json.ex
+++ lib/json.ex
@@ -4,20 +4,9 @@ defmodule JSON do
     JSON.Encode.to_json(item)
   end
 
-  def decode(<< ?", rest :: binary >>) do
-    decode_rest_of_string(rest, [])
-  end
-
-  def decode(_) do
-    raise "not implemented"
-  end
-
-  defp decode_rest_of_string("\"", acc) do
-    { :ok, to_binary(Enum.reverse(acc)) }
-  end
-
-  defp decode_rest_of_string(<< x, rest :: binary >>, acc) do
-    decode_rest_of_string(rest, [ x | acc ])
-  end
+  @doc """
+  Decode a String to an Erlang object
+  """
+  def decode(json_string), do: JSON.Decode.decode(json_string)
 
 end
