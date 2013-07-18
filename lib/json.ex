ebb9da8196d5ed41acc1a3074975157c43d2d438
Carlos Brito Lage <carlos@carloslage.net>
2013-07-17 23:13:14 -0400

    Documented JSON module, added the two decode implementations

--- lib/json.ex
+++ lib/json.ex
@@ -1,10 +1,35 @@
 defmodule JSON do
+  
+  @doc """
+  Returns a JSON string representation of the Elixir term
 
-  def encode(item) do
-    JSON.Encode.to_json(item)
+  ## Examples
+
+      iex> JSON.encode([result: "this will be a elixir result"]) 
+      "{\"result\":\"this will be a elixir result\"}"
+      
+  """
+  @spec encode(term) :: bitstring
+  def encode(term) do
+    JSON.Encode.to_json(term)
   end
 
+  @doc """
+  Converts a valid JSON string into an Elixir term
+
+  ## Examples
+
+      iex> JSON.decode("{\"result\":\"this will be a elixir result\"}") 
+      {:ok, [result: "this will be a elixir result"]}
+      
+  """
+  @spec decode(bitstring) :: {:ok, term}
   def decode(_) do
+    {:error, "not implemented"}
+  end
+  
+  @spec decode!(bitstring) :: term
+  def decode!(_) do
     raise "not implemented"
   end
 
