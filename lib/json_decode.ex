bcdbda07f0453f4607ba856eb31077bbc1474624
Carlos Brito Lage <carlos@carloslage.net>
2013-07-19 00:25:49 -0400

    general cleanup

--- lib/json_decode.ex
+++ lib/json_decode.ex
@@ -1,4 +1,8 @@
 defmodule JSON.Decode do
+
+  #32 = ascii space, cleaner than using "? ", I think
+  @acii_space 32
+
   defexception UnexpectedTokenError, token: nil do
     def message(exception) do
       "Invalid JSON - unexpected token >>#{exception.token}<<"
@@ -24,18 +28,18 @@ defmodule JSON.Decode do
     {root, remaining_bitstring} = consume_whitespace(bitstring) |> process_root_token
                                           
     #remaining_bitstring should be empty due to being in the root context otherwise this is invalid json
-    if ("" !== consume_whitespace(remaining_bitstring)) do 
+    unless "" === consume_whitespace(remaining_bitstring) do 
       raise UnexpectedTokenError, token: remaining_bitstring
     end
 
     root
   end
 
-   defp process_root_token(<< "{" , tail :: binary >>) do
+   defp process_root_token(<< ?{ , tail :: binary >>) do
     accept_object(tail)    
   end
 
-  defp process_root_token(<< "[" , tail :: binary >>) do
+  defp process_root_token(<< ?[ , tail :: binary >>) do
     accept_list(tail)
   end
 
@@ -51,9 +55,7 @@ defmodule JSON.Decode do
     raise "not implemented"
   end
 
-
-  # consume whitespace, 32 = ascii space
-  defp consume_whitespace(<< token :: utf8, tail :: binary>>) when token in [?\t, ?\r, ?\n, 32] do
+  defp consume_whitespace(<< token :: utf8, tail :: binary>>) when token in [?\t, ?\r, ?\n, @ascii_space] do
     consume_whitespace(tail)
   end
 
@@ -61,7 +63,7 @@ defmodule JSON.Decode do
     bitstring
   end
 
-  defp process_string_token(<< "\"" , tail :: binary >>) do
+  defp process_string_token(<< ?" , tail :: binary >>) do
     accept_string(tail, "")
   end
 
@@ -70,7 +72,7 @@ defmodule JSON.Decode do
   end
 
   #Stop condition for proper end of string
-  defp accept_string("\"", accumulator) do
+  defp accept_string(?", accumulator) do
     accumulator
   end
   
