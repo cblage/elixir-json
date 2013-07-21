eba31f694c2f592ca44db55ad9c421ed9f02ff30
James A. Rosen <james@zendesk.com>
2013-07-21 10:48:57 -0700

    remove unused decode helper methods

--- lib/json/decode.ex
+++ lib/json/decode.ex
@@ -134,38 +134,6 @@ defmodule JSON.Decode do
     { n, rest }
   end
 
-  #Accepts anything considered a root token (object or array for now)
-  defp accept_root(bitstring) do
-    {root, remaining_bitstring} = String.lstrip(bitstring) |> process_root_token
-
-    #remaining_bitstring should be empty due to being in the root context otherwise this is invalid json
-    unless "" === String.strip(remaining_bitstring) do
-      raise UnexpectedTokenError, token: remaining_bitstring
-    end
-
-    root
-  end
-
-  defp process_root_token(<< ?{ , tail :: binary >>) do
-    accept_object(tail)
-  end
-
-  defp process_root_token(<< ?[ , tail :: binary >>) do
-    accept_list(tail)
-  end
-
-  defp process_root_token(token) when is_bitstring(token) do
-    raise UnexpectedTokenError, token: token
-  end
-
-  defp accept_object(bitstring) when is_bitstring(bitstring) do
-    raise "not implemented"
-  end
-
-  defp accept_list(bitstring) when is_bitstring(bitstring) do
-    raise "not implemented"
-  end
-
   # String Parsing
 
   # Stop condition for proper end of string
