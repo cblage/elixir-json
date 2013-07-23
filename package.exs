389c2b02789ff606107e2c849a1572b951562c68
Carlos Brito Lage <carlos@carloslage.net>
2013-07-23 01:25:46 -0400

    added EXPM package definition

---
+++ package.exs
@@ -0,0 +1,11 @@
+Expm.Package.new(
+  name: "json", 
+  description: "Native Elixir library for JSON encoding and decoding",
+  version: "0.1.0", 
+  keywords: ["Elixir", "json", "encoder", "decoder", "encode", "decode"], 
+  licenses: [[name: "BSD 3-Clause"]],
+  maintainers: [
+    [name: "Carlos Brito Lage",  email: "carlos@carloslage.net"]
+    [name: "James Alexander Rosen", email: "james.a.rosen@gmail.com"]],
+  repositories: [[github: "cblage/elixir-json"]])
+    
