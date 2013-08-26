7ef845dac311f916f6d045df18075314593c1e15
Carlos Brito Lage <carlos@carloslage.net>
2013-08-27 00:17:26 +0100

    changed supported elixir version to 0.10.2-dev

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule JSON.Mixfile do
   def project do
     [ app: :json,
       version: "0.2.0",
-      elixir: "~> 0.10.0",
+      elixir: "~> 0.10.2-dev",
       deps: deps,
       source_url: "https://github.com/cblage/elixir-json" ]
   end
