9bac6d4767170bc71b91b8548d8dc8eb19fa938a
Carlos Brito Lage <carlos@carloslage.net>
2013-09-25 21:48:05 +0100

    Bumped version number

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule JSON.Mixfile do
 
   def project do
     [ app: :json,
-      version: "0.2.3",
+      version: "0.2.4",
       elixir: "~> 0.10.2",
       deps: deps,
       source_url: "https://github.com/cblage/elixir-json" ]
