5696c7cd17cd3c2be0850e4ca9e0af6f34dc114c
Carlos Brito Lage <carlos@carloslage.net>
2014-03-05 23:43:45 -0500

    Base Array collector implementation that provides a basic implementation for the "close" function

---
+++ lib/json/collector/array/behaviour.ex
@@ -0,0 +1,11 @@
+defmodule JSON.Collector.Array.Behaviour do 
+  defmacro __using__(_) do
+    quote do
+      @behaviour JSON.Collector.Array
+
+      def close(acc), do: acc
+
+      defoverridable close: 1
+    end
+  end
+end
