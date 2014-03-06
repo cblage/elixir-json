efdb5960753ff50aeb6874c486599134a138fbc8
Carlos Brito Lage <carlos@carloslage.net>
2014-03-05 23:37:39 -0500

    better test coverage for charlist decoding

--- test/json_decode_test.exs
+++ test/json_decode_test.exs
@@ -3,9 +3,12 @@ Code.require_file "test_helper.exs", __DIR__
 defmodule JSONDecodeTest do
 
   defmodule DSL do
+    def prefix(bitstring) when is_bitstring(bitstring), do: "bitstring containing "
+    def prefix(bitstring) when is_list(bitstring), do: "charlist containing "
+
     defmacro decodes(name, input, output) do
       quote do
-        test "decodes " <> unquote(name) do
+        test "decodes " <> prefix(unquote(input)) <> unquote(name) do
           decode_result = JSON.decode(unquote(input))
           case decode_result do
             { :ok, actual } -> assert unquote(output) == actual
@@ -17,7 +20,7 @@ defmodule JSONDecodeTest do
 
     defmacro cannot_decode(name, input, error_info) do
       quote do
-        test "cannot decode " <> unquote(name) do
+        test "cannot decode " <> prefix(unquote(input)) <> unquote(name) do
           decode_result = JSON.decode(unquote(input))
           case decode_result do
             { :error, actual } -> assert unquote(error_info) == actual
@@ -28,7 +31,7 @@ defmodule JSONDecodeTest do
     end
   end
 
-  defmodule Cases do
+  defmodule BitstringCases do
     use ExUnit.Case
     import JSONDecodeTest.DSL
 
@@ -39,14 +42,14 @@ defmodule JSONDecodeTest do
     decodes "empty string", "\"\"", ""
     decodes "simple string", "\"this is a string\"", "this is a string"
     decodes "unicode string", "\"µ¥ ß†®îñ©\"",  "µ¥ ß†®îñ©"
-    decodes "string with quotes", "\"I said, \\\"Hi.\\\"\"", "I said, \"Hi.\""
-    decodes "string with solidi", "\"\\/ \\\\\"", "/ \\"
+    decodes "quotes", "\"I said, \\\"Hi.\\\"\"", "I said, \"Hi.\""
+    decodes "solidi", "\"\\/ \\\\\"", "/ \\"
 
-    decodes "string with control characters",
+    decodes "control characters",
             "\"tab\\tnewline\\ncarriage return\\rform feed\\fend\"",
             "tab\tnewline\ncarriage return\rform feed\fend"
 
-    decodes "string with unicode escape", "\"star -> \\u272d <- star\"", "star -> ✭ <- star"
+    decodes "unicode escape", "\"star -> \\u272d <- star\"", "star -> ✭ <- star"
 
     decodes "positive integer", "1337", 1337
     decodes "positive float", "13.37", 13.37
@@ -65,29 +68,7 @@ defmodule JSONDecodeTest do
     decodes "simple array", "[ 1, 2, \"three\", 4 ]", [ 1, 2, "three", 4 ]
     decodes "nested array", " [ null, [ false, \"five\" ], [ 3, true ] ] ", [nil, [false, "five"], [3, true]]
 
-    decodes "complex object in charlist",
-            '{
-              "name": "Jenny",
-              "active": true,
-              "phone": "1.415.555.0000",
-              "balance": 1.52E+5,
-              "children": [
-                { "name": "Penny" },
-                { "name": "Elga" }
-              ]
-             }',
-             HashDict.new([
-              { "name", "Jenny" },
-              { "active", true },
-              { "phone", "1.415.555.0000" },
-              { "balance", 1.52e+5 },
-              { "children", [
-                HashDict.new([ { "name", "Penny" } ]),
-                HashDict.new([ { "name", "Elga" } ])
-              ] }
-            ])
-
-    decodes "complex object in bitstring",
+    decodes "complex object",
             "{
               \"name\": \"Rafaëlla\",
               \"active\": true,
@@ -113,7 +94,7 @@ defmodule JSONDecodeTest do
 
     cannot_decode "unterminated string", "\"Not a full string", :unexpected_end_of_buffer
 
-    cannot_decode "string with bad Unicode escape", "\"bzzt: \\u27qp wrong\"", {:unexpected_token, "qp wrong\""}
+    cannot_decode "bad Unicode escape", "\"bzzt: \\u27qp wrong\"", {:unexpected_token, "qp wrong\""}
 
     cannot_decode "number with trailing .", "889.foo", {:unexpected_token, ".foo"}
 
@@ -126,6 +107,79 @@ defmodule JSONDecodeTest do
     cannot_decode "multiple value unterminated object", "{\"foo\":\"bar\", \"omg\":", :unexpected_end_of_buffer
 
     cannot_decode "object with missing colon", "{\"foo\" \"bar\"}", {:unexpected_token, "\"bar\"}"}
+  end
+
+  defmodule CharlistCases do
+    use ExUnit.Case
+    import JSONDecodeTest.DSL
+
+    decodes "null",  'null',  nil
+    decodes "true", 'true',  true
+    decodes "false", 'false', false
+
+    decodes "empty string", '""', ""
+    decodes "simple string", '"this is a string"', "this is a string"
+  
+    decodes "string with quotes", '"I said, \\"Hi.\\""', "I said, \"Hi.\""
+    
+    decodes "string with unicode escape", '"star -> \\u272d <- star"', "star -> ✭ <- star"
+
+    decodes "positive integer", '1337', 1337
+    decodes "positive float", '13.37', 13.37
+    decodes "negative integer", '-1337', -1337
+    decodes "negative float", '-13.37', -13.37
+
+    decodes "integer with exponent", '98e2', 9800
+    decodes "float with positive exponent", '-1.22783E+4', -12278.3
+    decodes "float with negative exponent", '903.4e-6', 0.0009034
+
+    decodes "empty object", "{}", HashDict.new
+    decodes "simple object", '{"result": "this is awesome"}', HashDict.new([ { "result", "this is awesome" } ])
+
+    decodes "empty array", '  [   ] ', []
+    decodes "simple array", '[ 1, 2, "three", 4 ]', [ 1, 2, "three", 4 ]
+    decodes "nested array", ' [ null, [ false, "five" ], [ 3, true ] ] ', [nil, [false, "five"], [3, true]]
+
+    decodes "complex object",
+            '{
+              "name": "Jenny",
+              "active": true,
+              "phone": "1.415.555.0000",
+              "balance": 1.52E+5,
+              "children": [
+                { "name": "Penny" },
+                { "name": "Elga" }
+              ]
+             }',
+             HashDict.new([
+              { "name", "Jenny" },
+              { "active", true },
+              { "phone", "1.415.555.0000" },
+              { "balance", 1.52e+5 },
+              { "children", [
+                HashDict.new([ { "name", "Penny" } ]),
+                HashDict.new([ { "name", "Elga" } ])
+              ] }
+            ])
+
+    
+    cannot_decode "bad literal", 'nul', {:unexpected_token, 'nul'}
+
+    cannot_decode "unterminated string", '"Not a full string', :unexpected_end_of_buffer
+
+    cannot_decode "string with bad Unicode escape", '"bzzt: \\u27qp wrong"', {:unexpected_token, 'qp wrong\"'}
+
+    cannot_decode "number with trailing .", '889.foo', {:unexpected_token, '.foo'}
+
+    cannot_decode "open brace", '{', :unexpected_end_of_buffer
+
+    cannot_decode "bad object", '{foo', {:unexpected_token, 'foo'}
+
+    cannot_decode "unterminated object", '{"foo":"bar"', :unexpected_end_of_buffer
+    
+    cannot_decode "multiple value unterminated object", '{"foo":"bar", "omg":', :unexpected_end_of_buffer
+
+    cannot_decode "object with missing colon", '{"foo" "bar"}', {:unexpected_token, '"bar"}'}
   end
 
 end
