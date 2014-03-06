e25085798a05f0ac818cdfd92d3d527c03e852d2
Carlos Brito Lage <carlos@carloslage.net>
2014-03-05 23:43:59 -0500

    Base Object collector implementation that provides a basic implementation for the "close" function

---
+++ lib/json/collector/object/behaviour.ex
@@ -0,0 +1,11 @@
+defmodule JSON.Collector.Object.Behaviour do 
+  defmacro __using__(_) do
+    quote do
+      @behaviour JSON.Collector.Object
+
+      def close(acc), do: acc
+
+      defoverridable close: 1
+    end
+  end
+end
