9c0fe95ca5308bb91eaa5d5464805cfed9af812c
Carlos Brito Lage <carlos@carloslage.net>
2016-01-28 13:04:34 -0500

    preparing 0.3.3 release

--- package.exs
+++ package.exs
@@ -1,10 +1,9 @@
 Expm.Package.new(
   name: "json",
   description: "Native Elixir library for JSON encoding and decoding",
-  version: "0.3.2",
+  version: "0.3.3",
   keywords: ["Elixir", "json", "encoder", "decoder", "encode", "decode"],
   licenses: [[name: "BSD 3-Clause"]],
   maintainers: [
-    [name: "Carlos Brito Lage",  email: "carlos@carloslage.net"],
-    [name: "James Alexander Rosen", email: "james.a.rosen@gmail.com"]],
-  repositories: [[github: "cblage/elixir-json", tag: "v0.3.2"]])
+    [name: "Carlos Brito Lage",  email: "carlos@carloslage.net"]],
+  repositories: [[github: "cblage/elixir-json", tag: "v0.3.3"]])
