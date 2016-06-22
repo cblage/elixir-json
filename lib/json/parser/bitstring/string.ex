e486b91fb0ae9f005dcc8c98f22dd06422fcd9eb
danturn <dan.turner@cloudhouse.com>
2016-06-22 14:17:17 +0100

    removing warnings

--- lib/json/parser/bitstring/string.ex
+++ lib/json/parser/bitstring/string.ex
@@ -83,7 +83,7 @@ defmodule JSON.Parser.Bitstring.String do
   # Parsing sugorrogate pairs
   # http://unicodebook.readthedocs.org/unicode_encodings.html#utf-16-surrogate-pairs
   # Inspired by Poison's function
-  defp parse_escaped_unicode_codepoint(<< ?d, hex :: utf8, f1, f2 , ?\\, ?u, ?d, hex2:: utf8, s1, s2, json :: binary >>, acc, 0)
+  defp parse_escaped_unicode_codepoint(<< ?d, hex :: utf8, f1, f2 , ?\\, ?u, ?d, hex2:: utf8, s1, s2, json :: binary >>, _, 0)
   when (hex >= 56) do
     first_part = (List.to_integer( [?d, hex, f1, f2], 16) &&& 1023) <<< 10
     second_part = List.to_integer( [?d, hex2, s1, s2], 16) &&& 1023
