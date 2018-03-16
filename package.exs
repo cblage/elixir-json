614974b05efdcf847058b34960b40801cebc89d1
Carlos Brito Lage <carlos@carloslage.net>
2018-03-16 19:57:11 +0000

    Updated Authors

--- package.exs
+++ package.exs
@@ -1,7 +1,7 @@
 Expm.Package.new(
   name: "json",
   description: "Native Elixir library for JSON encoding and decoding",
-  version: "1.0.2",
+  version: "1.0.3-SNAPSHOT",
   keywords: ["Elixir", "json", "encoder", "decoder", "encode", "decode"],
   licenses: [[name: "BSD 3-Clause"]],
   maintainers: [
