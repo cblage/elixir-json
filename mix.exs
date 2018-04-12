9a9f6499ad2de2dfd3294c16c8ad601e57b4d781
Carlos Brito Lage <carlos@carloslage.net>
2018-04-12 01:12:40 +0100

    version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ElixirJSON_120_SNAPSHOT.Mixfile do
   use Mix.Project
 
-  @version "2.1.0-SNAPSHOT"
+  @version "1.2.0"
 
   def project do
     [
--- package.exs
+++ package.exs
@@ -1,7 +1,7 @@
 Expm.Package.new(
   name: "json",
   description: "Native Elixir library for JSON encoding and decoding",
-  version: "2.1.0-SNAPSHOT",
+  version: "1.2.0",
   keywords: ["Elixir", "json", "encoder", "decoder", "encode", "decode"],
   licenses: [[name: "BSD 3-Clause"]],
   maintainers: [
