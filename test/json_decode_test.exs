803ece7e84bb786d12bbc95ed4fc929aa46a1ce8
Carlos Brito Lage <carlos@carloslage.net>
2018-04-13 01:45:04 +0100

    credo

--- test/json_decode_test.exs
+++ test/json_decode_test.exs
@@ -232,7 +232,7 @@ defmodule JSONDecodeTest do
 
     decodes("simple object string keys" , '{"foo" : 123}', %{"foo" => 123})
 
-    decodes("simple object containing array" , '{"foo" : [1,2,3]}', %{"foo" => [1,2,3]})
+    decodes("simple object containing array" , '{"foo" : [1,2,3]}', %{"foo" => [1,2,3,]})
 
     decodes("simple object containing big array" ,
       '{
@@ -242,9 +242,8 @@ defmodule JSONDecodeTest do
                        3
                      ]
       }',
-      %{"foo" => [1,2,3]}
+      %{"foo" => [1,2,3,]}
     )
-
 
     decodes(
       "complex object",
