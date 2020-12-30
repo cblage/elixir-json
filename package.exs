845e2cd8e48f0cbd783aad71af5f32960277906e
Carlos Brito Lage <carlos@carloslage.net>
2020-12-30 21:11:38 +0000

    bump

--- package.exs
+++ package.exs
@@ -1,7 +1,7 @@
 Expm.Package.new(
   name: "json",
   description: "Native Elixir library for JSON encoding and decoding",
-  version: "1.4.0",
+  version: "1.4.1",
   keywords: ["Elixir", "json", "encoder", "decoder", "encode", "decode", "protocol"],
   licenses: [[name: "BSD 3-Clause"]],
   maintainers: [
