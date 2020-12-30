b6882d241ec3c566acfd8987a3c57033935c43e2
Carlos Brito Lage <carlos@carloslage.net>
2020-12-30 19:13:14 +0000

    Fix warning, make minimum Elixir version 1.7

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule ElixirJSON_140_SNAPSHOT.Mixfile do
     [
       app: :json,
       version: @version,
-      elixir: "~> 1.5",
+      elixir: "~> 1.7",
       deps: deps(Mix.env()),
       description: "The First Native Elixir library for JSON encoding and decoding",
       package: package(),
@@ -35,9 +35,9 @@ defmodule ElixirJSON_140_SNAPSHOT.Mixfile do
 
   def deps(_) do
     [
-      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:dialyzex, "~> 1.2", only: [:dev]},
-      {:excoveralls, "~> 0.8", only: :test, optional: true, runtime: false}
+      {:excoveralls, "~> 0.13.4", only: :test, optional: true, runtime: false}
     ]
   end
 
